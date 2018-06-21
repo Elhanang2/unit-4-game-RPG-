0f181986cb538ee86e92cdda787515a93a4cbc9b
Ayna <aynazewd@yahoo.com>
2018-06-20 23:34:44 -0700

    js to move the characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,51 +2,81 @@ var a=$(".charactor1");
 var b=$(".charactor2");
 var c=$(".charactor3");
 var d=$(".charactor4");
-var x,y,z;
-/*var w=$(".charactorp1");
-var x=$(".charactorp2");
-var y=$(".charactorp3");
-var z=$(".charactorp4");
-var i=$(".charactord1");
-var j=$(".charactord2");
-var k=$(".charactord3");
-var l=$(".charactord4");*/
+var x,y,z,w;
 
-/*for(var  i=0;i<1; i++){
-    a.click(function(){
-    b.hide(),
-    c.hide(),
-    d.hide(),
-  x.show(),
-  y.show(),
-  z.show();
-    
-});}
-x.click(function(){
-    x.hide(),j.show()
-});*/
 
 a.on("click",function(){
     /*b.animate({top:"200px"},"normal");*/
-    movement(b,c,d);
+    movement(a,b,c,d);
+    b.on("click",function(){
+        $(".defender-image").append(b.remove()),
+        b.css({"background-color":"black","font-size":"15px","text-align":"center","color":"white"})
+    });
+    c.on("click",function(){
+        $(".defender-image").append(c.remove()),
+        c.css({"background-color":"black","font-size":"15px","text-align":"center","color":"white"})
+    });
+    d.on("click",function(){
+        $(".defender-image").append(d.remove()),
+        d.css({"background-color":"black","font-size":"15px","text-align":"center","color":"white"})
+    });
   
 });
+
 b.on("click",function(){
-    movement(a,c,d);
+    movement(b,a,c,d);
+    a.on("click",function(){
+        $(".defender-image").append(a.remove()),
+        a.css({"background-color":"black","font-size":"15px","text-align":"center","color":"white"})
+    });
+    c.on("click",function(){
+        $(".defender-image").append(c.remove()),
+        c.css({"background-color":"black","font-size":"15px","text-align":"center","color":"white"})
+    });
+    d.on("click",function(){
+        $(".defender-image").append(d.remove()),
+        d.css({"background-color":"black","font-size":"15px","text-align":"center","color":"white"})
+    });
 });
 c.on("click",function(){
-    movement(a,b,d);
+    movement(c,a,b,d);
+    a.on("click",function(){
+        $(".defender-image").append(a.remove()),
+        a.css({"background-color":"black","font-size":"15px","text-align":"center","color":"white"})
+    });
+    b.on("click",function(){
+        $(".defender-image").append(b.remove()),
+        b.css({"background-color":"black","font-size":"15px","text-align":"center","color":"white"})
+    });
+    d.on("click",function(){
+        $(".defender-image").append(d.remove()),
+        d.css({"background-color":"black","font-size":"15px","text-align":"center","color":"white"})
+    });
 });
 d.on("click",function(){
-    movement(a,b,c);
+    movement(d,a,b,c);
+    a.on("click",function(){
+        $(".defender-image").append(a.remove()),
+        a.css({"background-color":"black","font-size":"15px","text-align":"center","color":"white"})
+    });
+    b.on("click",function(){
+        $(".defender-image").append(b.remove()),
+        b.css({"background-color":"black","font-size":"15px","text-align":"center","color":"white"})
+    });
+    c.on("click",function(){
+        $(".defender-image").append(c.remove()),
+        c.css({"background-color":"black","font-size":"15px","text-align":"center","color":"white"})
+    });
 });
 
 /*$("#photoholder").append($(this).children("img").remove());*/
-function movement(x,y,z){  
-    $(".charactor-pic").append(x.remove()),
+function movement(w,x,y,z){ 
+    $(".charactor-pic").append(w.remove()),
+ 
+    $(".enemy-pic").append(x.remove()),
  x.css({"background-color":"red","font-size":"15px","text-align":"center"}),
-  $(".charactor-pic").append(y.remove()),
+  $(".enemy-pic").append(y.remove()),
   y.css({"background-color":"red","font-size":"15px","text-align":"center"}),
-  $(".charactor-pic").append(z.remove());
+  $(".enemy-pic").append(z.remove());
   z.css({"background-color":"red","font-size":"15px","text-align":"center"})
-return(x,y,z);}
+return(w,x,y,z);}
