93f7bbb8492125b9ac0e9c1bfc1ff9fa5fdeeefd
Ayna <aynazewd@yahoo.com>
2018-06-22 19:17:37 -0700

    To display defender and attacker score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,30 +4,48 @@ var c=$(".charactor3");
 var d=$(".charactor4");
 var x,y,z,w;
 var j,k,l;
-var ran;
-var char=6;
+var m,n,o,p;
+var f,g,h;
+var chartotal;
+var defendtotal;
+var char=0;
+var attack;
+var hp1,hp2,hp3,hp4;
+var h1=$(".hp1");
+var h2=$(".hp2");
+var h3=$(".hp3");
+var h4=$(".hp4");
 a.on("click",function(){
     /*b.animate({top:"200px"},"normal");*/
-    movement(a,b,c,d);
-    defender(b,c,d);
+    movement(a,b,c,d,120);
+    defender(b,c,d,100,150,180,h2,h3,h4,h1);
+    randomfn();
+    console.log(attack);
+    
+    
+    
+    
 });
 
 b.on("click",function(){
-    movement(b,a,c,d);
-    defender(a,c,d)
+    movement(b,a,c,d,100);
+    defender(a,c,d,120,150,180,h1,h3,h4,h2);
+    randomfn();
     
 });
 c.on("click",function(){
-    movement(c,a,b,d);
-    defender(a,b,d)
+    movement(c,a,b,d,150);
+    defender(a,b,d,120,100,180,h1,h2,h4,h3);
+    randomfn();
 });
 d.on("click",function(){
-    movement(d,a,b,c);
-    defender(a,b,c)
+    movement(d,a,b,c,180);
+    defender(a,b,c,120,100,150,h1,h2,h3,h4);
+    randomfn();
 });
 
 /*$("#photoholder").append($(this).children("img").remove());*/
-function movement(w,x,y,z){ 
+function movement(w,x,y,z,v){ 
     $(".charactor-pic").append(w.remove()),
  
     $(".enemy-pic").append(x.remove()),
@@ -36,35 +54,62 @@ function movement(w,x,y,z){
   y.css({"background-color":"red","font-size":"15px","text-align":"center"}),
   $(".enemy-pic").append(z.remove());
   z.css({"background-color":"red","font-size":"15px","text-align":"center"})
+  chartotal=v;
 return(w,x,y,z);}
-function defender(j,k,l){
+function defender(j,k,l,q,r,s,f,g,h,hp){
     
     j.on("click",function(){
         $(".defender-image").append(j.remove()),
         j.css({"background-color":"black","font-size":"15px","text-align":"center","color":"white"})
+        defendtotal=q;
+        
+        console.log(p);
+        res(hp,f);
     });
     k.on("click",function(){
         $(".defender-image").append(k.remove()),
         k.css({"background-color":"black","font-size":"15px","text-align":"center","color":"white"})
+        defendtotal=r;
+       
+        res(hp,g);
     });
     l.on("click",function(){
         $(".defender-image").append(l.remove()),
         l.css({"background-color":"black","font-size":"15px","text-align":"center","color":"white"})
+        defendtotal=s;
+        
+        res(hp,h);
     });
-    return(j,k,l);
+    return(j,k,l,q,r,s,f,g,h,hp2,hp3,hp4);
 }
 $(".attack").on("click",function(){
 
 });
-randomfn();
-$(".attack").on("click",function(){
-    char=char+6;
-    $(".charactor-title").p.text(x-ran)
-    $(".defender").p.text(x-ran);
 
-});
+
 
 function randomfn(){
-    ran=Math.floor((Math.random()*5)+1)*5;
+    attack=Math.floor((Math.random()*5)+1)*5;
     
+}
+function res(m,o){
+    if((chartotal>=7) && (defendtotal>=0)){
+        $(".attack").on("click",function(){
+            char=char+6;
+            defendtotal=defendtotal-char;
+            console.log(defendtotal);
+             chartotal=chartotal-attack;
+            console.log(chartotal);
+           m.text(chartotal);
+        o.text(defendtotal);
+           return(m,n); 
+            
+        });
+    }else if((chartotal<0) && (defendtotal>0)){
+        $("<p>").text("you loss"); 
+        console.log("you loss");
+    }
+    else if((chartotal>0) && (defendtotal<0)){
+        $("<p>").text("you loss"); 
+        console.log("you win");}
 }
